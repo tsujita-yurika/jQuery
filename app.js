5a2669d698c0a8c5ad0c5df9241ba669ae459e47
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-12-17 07:20:50 +0000

    [start]jQuery

---
+++ app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  // bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
+  // $( セレクタbody ).メソッドhtml ( パラメータ<h1>Hello JQuery!!</h1> );
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
