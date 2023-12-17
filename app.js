7b4e36c7293b0fcf7ed5dec18fcd2587b3612ce9
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-12-17 08:11:14 +0000

    jQueryでHTMLのCSS要素を操作

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
-  // bodyタグ内のHTMLを、<h1>Hello JQuery!!</h1>で書き換えなさい
-  // $( セレクタbody ).メソッドhtml ( パラメータ<h1>Hello JQuery!!</h1> );
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
